d56c8328eb46f9f2c382270f88967457235390c4
alijon208 <andurahmonovmuhammadali@gmail.com>
2026-03-10 23:18:22 +0500

    3

--- js/main.js
+++ js/main.js
@@ -1,16 +1,18 @@
 const menu = document.getElementById("menu");
 const list = document.getElementById("h-list");
 const gradient = document.getElementById("gradient");
-const open = false;
+const body = document.querySelector("body")
 
 function menuOpen()  {
     menu.classList.toggle("menu-open");
     list.classList.toggle("d-block");
+    body.classList.toggle("body-hidden")
     gradient.style.display = "block"
 }
 
 gradient.addEventListener("click", () => {
     menu.classList.remove("menu-open");
     list.classList.remove("d-block");
+    body.classList.remove("body-hidden")
     gradient.style.display = "none"
 })
